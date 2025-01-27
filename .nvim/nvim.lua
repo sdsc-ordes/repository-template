6b74fb2dd274b4f5b6a35cafe3d6cceb91d9e23d
Gabriel Nützi <gnuetzi@gmail.com>
2025-01-27 19:16:54 +0100

    chore: detect jinja filetype without jinja

--- .nvim/nvim.lua
+++ .nvim/nvim.lua
@@ -7,3 +7,17 @@
 -- Needs https://github.com/ldelossa/nvim-dap-projects
 nvimdap = require("nvim-dap-projects")
 nvimdap.search_project_config()
+
+local group = vim.api.nvim_create_augroup("StripJinja", { clear = true })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.jinja",
+    group = group,
+    callback = function()
+        local fname = vim.fn.expand("%:t") -- Get the filename
+        local stripped_name = fname:gsub("%.jinja", "") -- Strip `.jinja`
+        local ext = stripped_name:match("%.(%w+)$") -- Extract the new extension
+        if ext then
+            vim.bo.filetype = ext -- Set the filetype
+        end
+    end,
+})
