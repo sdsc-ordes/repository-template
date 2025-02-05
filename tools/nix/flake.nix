043600dc623e640ac6ee24276037b8ca5a0ab5b9
Gabriel Nützi <gnuetzi@gmail.com>
2025-02-05 13:22:45 +0100

    fix: remove unneeded `copier` flake

--- tools/nix/flake.nix
+++ tools/nix/flake.nix
@@ -24,11 +24,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    copier = {
-      url = "github:copier-org/copier?ref=v9.4.1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     # Snowfall provides a structured way of creating a flake output.
     # Documentation: https://snowfall.org/guides/lib/quickstart/
     snowfall-lib = {
