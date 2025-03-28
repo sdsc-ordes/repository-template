98f227d98d23ecc87e4c2c2e500d1f98deadd113
Gabriel Nützi <gnuetzi@gmail.com>
2025-03-28 12:43:54 +0100

    fix: make toolchains easier

--- tools/nix/shells/format/default.nix
+++ tools/nix/shells/format/default.nix
@@ -9,10 +9,10 @@ lib.${namespace}.makeShell {
   inherit inputs;
   inherit (pkgs) system;
   modules = [
-    (args: {
+    {
       packages = [
         pkgs.${namespace}.treefmt
       ];
-    })
+    }
   ];
 }
