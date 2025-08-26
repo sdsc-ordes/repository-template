2e2655b46261c3237542fda8e6bcd9b355f57686
Gabriel Nützi <gnuetzi@gmail.com>
2025-08-26 09:05:54 +0200

    fix: remove `nix-list` for `nix::show` (#13)

--- src/generic/tools/nix/toolchains/toolchain-generic.nix
+++ src/generic/tools/nix/toolchains/toolchain-generic.nix
@@ -1,7 +1,10 @@
 # This function returns a attrset of `devenv` modules
 # which can be passed to `mkShell`.
 {
-  self',
+  self', # The own flake, where `system` is preselected.
+  inputs', # All inputs where `system` is preselected.
+  pkgs, # The unstable packages from the input `nixpkgs`.
+  pkgsStable, # The stable packages from the inputs `nixpkgs-stable`.
   ...
 }:
 let
