c4f72725ce0c86f0c84eb027ab98d9cfe9088f64
Gabriel Nützi <gnuetzi@gmail.com>
2025-10-01 18:23:24 +0200

    fix: changelog

--- tools/nix/packages/changelog.parts.nix
+++ tools/nix/packages/changelog.parts.nix
@@ -6,13 +6,19 @@
   perSystem =
     { pkgs, ... }:
     {
-      # Generate a changelog.
+      # Generate a changelog from `HEAD` to the last tag on the current branch.
+      # With the following arguments.
+      # `$1: <new-tag>`
+      # `$2: <git-cliff-config>` (optional)
+      # `$3`: <changelog-file> (optional)
+      # NOTE: Due to Nix you need to escape with `''${VAR}`
       packages.generate-changelog = pkgs.writeShellApplication {
         name = "generate-changelog";
         runtimeInputs = [
           pkgs.git
           pkgs.gnugrep
           pkgs.coreutils
+          pkgs.git-cliff
         ];
 
         text =
@@ -23,8 +29,8 @@
             cd "$root_dir"
 
             tag="$1"
-            config="$2"
-            file="$3"
+            config="''${2:-tools/config/git-cliff/config.toml}"
+            file="''${3:-CHANGELOG.md}"
 
             lastTag=$(git describe --abbrev=0 --tags)
             if [ "$lastTag" = "" ]; then
@@ -37,24 +43,33 @@
             start=HEAD
             end="$lastTag"
 
-            echo "Changelog in $end..$start" >&2
+            echo "Changelog in '$end..$start'" >&2
 
             if ! grep -q '<!-- next-content -->' "$file"; then
                 echo "no '<!-- next-content -->' tag in '$file'"
                 exit 1
             fi
 
+            non_first_parent_commits=()
             readarray -t non_first_parent_commits < <(
-                comm -23 <(git rev-list $end..$start | sort) \
-                         <(git rev-list --ancestry-path --first-parent $end..$start |sort)
+                comm -23 <(git rev-list "$end..$start" | sort) \
+                         <(git rev-list --ancestry-path --first-parent "$end..$start" |sort)
             )
+
+            skip_args=()
+            if [ "''${#non_first_parent_commits[@]}" -ne 0 ]; then
+              # shellcheck disable=SC2086,SC2206
+              skip_args=(--skip-commit)
+              skip_args+=("''${non_first_parent_commits[@]}")
+            fi
 
             out=$(git-cliff --config "$config" \
                 "$end..$start" \
                 --strip header \
-                --skip-commit $non_first_parent_commits \
+                "''${skip_args[@]}" \
                 --tag "$tag")
 
+            # Replace in file.
             echo "$out" | sed -i '/<!-- next-content -->/{
                 r /dev/stdin
                 d
