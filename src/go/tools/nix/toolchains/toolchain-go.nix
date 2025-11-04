57435951add6150a2922bd06e5a1fe379be3b68c
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2025-11-04 10:31:01 +0100

    fix: go/python

--- src/go/tools/nix/toolchains/toolchain-go.nix
+++ src/go/tools/nix/toolchains/toolchain-go.nix
@@ -6,31 +6,46 @@
   pkgs,
   ...
 }:
+let
+  package = pkgs.go_1_25;
+in
 {
   go = [
-    {
-      packages = [
-        # Go.
-        pkgs.go_1_23
+    (
+      { config, ... }:
+      {
+        packages = [
+          # Go.
+          package
 
-        # Go debugger.
-        pkgs.delve
-        # Language server.
-        pkgs.gopls
-        # Formatting
-        pkgs.golines
-        # Formatting (goimports)
-        pkgs.gotools
-        # Linting
-        pkgs.golangci-lint
-        pkgs.golangci-lint-langserver
+          # Go debugger.
+          pkgs.delve
+          # Language server.
+          pkgs.gopls
+          # Formatting
+          pkgs.golines
+          # Formatting (goimports)
+          pkgs.gotools
 
-        # Debugging
-        pkgs.lldb_18
-      ];
-      enterShell = ''
-        just setup
-      '';
-    }
+          # Linting
+          pkgs.golangci-lint
+          pkgs.golangci-lint-langserver
+
+          # Debugging
+          pkgs.lldb_18
+        ];
+
+        hardeningDisable = "fortify";
+
+        env.GOROOT = package + "/share/go/";
+        env.GOPATH = config.env.DEVENV_STATE + "/go";
+        env.GOTOOLCHAIN = "local";
+
+        enterShell = ''
+          export PATH=$GOPATH/bin:$PATH
+          just setup
+        '';
+      }
+    )
   ];
 }
