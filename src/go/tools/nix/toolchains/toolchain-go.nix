62f12528778c9af6849aadc466dce72a830ca477
Gabriel Nützi <gnuetzi@gmail.com>
2025-12-11 22:58:35 +0100

    fix: go

--- src/go/tools/nix/toolchains/toolchain-go.nix
+++ src/go/tools/nix/toolchains/toolchain-go.nix
@@ -12,25 +12,25 @@ in
 {
   go = [
     {
-      # repotemp.languages.go = {
-      #   enable = true;
-      #   inherit package;
-      #
-      #   tools.packages = [
-      #     # Go specific tools which should be compiled with the chosen
-      #     # go `package`.
-      #   ];
-      #
-      # };
+      repotemp.languages.go = {
+        enable = true;
+        inherit package;
 
-      #   packages = [
-      #     pkgs.golangci-lint
-      #     pkgs.golangci-lint-langserver
-      #   ];
-      #
-      #   enterShell = ''
-      #     just setup
-      #   '';
+        tools.packages = [
+          # Go specific tools which should be compiled with the chosen
+          # go `package`. Default is set to `tools.packagesDefaults`.
+        ];
+
+      };
+
+      packages = [
+        pkgs.golangci-lint
+        pkgs.golangci-lint-langserver
+      ];
+
+      enterShell = ''
+        just setup
+      '';
     }
   ];
 }
