59dfe589b419a25cd00cb8cc80bc7f6782d4f35a
Gabriel Nützi <gnuetzi@gmail.com>
2026-05-13 21:28:55 +0200

    feat: add rust release toolchain for packaging (#25)

--- src/rust/tools/nix/packages/rust-toolchain.parts.nix
+++ src/rust/tools/nix/packages/rust-toolchain.parts.nix
@@ -4,7 +4,11 @@
   ...
 }:
 let
+  # This should be used in a devShell.
   toolchainFile = self.lib.fs.repoRoot + "/tools/configs/rust/rust-toolchain.toml";
+
+  # This should be used for building a rust Nix derivation.
+  toolchainFileRelease = self.lib.fs.repoRoot + "/tools/configs/rust/rust-toolchain-release.toml";
 in
 {
   perSystem =
@@ -15,5 +19,6 @@ in
     in
     {
       packages.rust-toolchain = pkgsRust.pkgsBuildHost.rust-bin.fromRustupToolchainFile toolchainFile;
+      packages.rust-toolchain-release = pkgsRust.pkgsBuildHost.rust-bin.fromRustupToolchainFile toolchainFileRelease;
     };
 }
